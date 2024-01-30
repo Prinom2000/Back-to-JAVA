c6d5b629305a9a8de5f8c496ad015682016533d8
Prinom Mojumder <130492223+Prinom2000@users.noreply.github.com>
2024-01-31 01:37:39 +0600

    Add files via upload

---
+++ Prduct.java
@@ -0,0 +1,23 @@
+
+package Product;
+
+public class Prduct {
+    public static void main(String[] args){
+        int id;
+        String title;
+        int price;
+        String description;
+        String category;
+        id= 101;
+        title= "Mobile";
+        price= 100000 ;
+        description= "Valo Phone";
+        category= "Android";
+        
+        System.out.println(id);
+        System.out.println(title);
+        System.out.println(price);
+        System.out.println(description);
+        System.out.println(category);
+    }
+}
