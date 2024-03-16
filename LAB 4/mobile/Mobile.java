4930e82484853e263718ac27afac45aba40fe095
Prinom Mojumder <130492223+Prinom2000@users.noreply.github.com>
2024-03-17 01:32:47 +0600

    Add files via upload

---
+++ LAB 4/mobile/Mobile.java
@@ -0,0 +1,12 @@
+package mobile;
+
+public class Mobile {
+
+    String brand;
+    int price;
+    static String name;
+
+    public void show() {
+        System.out.println(brand + " : " + price + " : " + name);
+    }
+}
